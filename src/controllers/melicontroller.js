2d7c4ee146622c33ff453935d74c711382e6531e
Francisco Carbonelli <carbonelli.francisco@gmail.com>
2021-07-30 01:13:18 -0300

    Published products

--- src/controllers/melicontroller.js
+++ src/controllers/melicontroller.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const fs = require("fs")
+const csv = require("fast-csv")
+const path = require('path');
 
 const meliController = {
 
@@ -35,14 +38,120 @@ const meliController = {
         let URL = "https://api.mercadolibre.com/oauth/token"
 
         const response = await axios.post(URL, data, config)
+
         console.log(response.data.access_token)
         res.cookie("token", response.data.access_token, {  httpOnly: true })
         res.cookie('meli', '', { maxAge: 1 }); //Elimino la cookie anterior
+        res.redirect("/upload")
+    },
+    postProducts: async (req, res) => {
+        const file = req.file
+        if (!file) {
+            const error = new Error('Please upload a file')
+            error.httpStatusCode = 400
+            res.redirect("/upload")
+            //res.status(400).json(error);
+        }
+        console.log(file.originalname.split(".").pop())
+        if (file.originalname.split(".").pop() != "csv") {
+            const error = new Error('Please upload a CSV file')
+            error.httpStatusCode = 400
+            //res.status(400).json(error);
+            res.redirect("/upload")
+        }
+
+        fs.createReadStream(path.resolve(__dirname, "../uploads", 'tobsfile.csv'))
+        .pipe(csv.parse( {headers: true} ))
+        .on("error", error => console.error(error))
+        .on("data", data => publishItem2(data, req.cookies.token))
+       
+        fs.unlinkSync(path.resolve(__dirname, "../uploads", 'tobsfile.csv'));
+        
         res.redirect("/upload")
     }
     
 }
 
+const publishItem = async (data, token) => {  
 
+    let config = {
+        headers: {
+            'Authorization': 'Bearer '+ token
+        }
+    }
+
+    let melidata = {
+        "title":data.name,
+        "category_id":"MLA6049",
+        "price":data.price,
+        "currency_id":data.currency,
+        "available_quantity":data.qty,
+        "buying_mode":"buy_it_now",
+        "condition":data.condition,
+        "listing_type_id":"gold_special",
+        "pictures":[
+            {
+                "source":data.image
+            }
+        ],
+        "attributes":[
+            {
+                "id":"EAN",
+                "value_name":data.EAN
+            }
+        ]}
+
+        let URL = "https://api.mercadolibre.com/items"
+
+        try {
+            await axios.post(URL, melidata, config)
+            //console.log(melidata)
+        } catch (error) {
+            console.log(error)
+        }
+};
+
+const publishItem2 = async (data, token) => {  
+
+    axios.post("https://api.mercadolibre.com/items", {
+        "title":data.name,
+        "category_id":"MLA6049",
+        "price":data.price,
+        "currency_id":data.currency,
+        "available_quantity":data.qty,
+        "buying_mode":"buy_it_now",
+        "condition":data.condition,
+        "listing_type_id":"gold_special",
+        "pictures":[
+            {
+                "source":data.image
+            }
+        ],
+        "attributes":[
+            {
+                "id":"BRAND",
+                "value_name":"Marca del producto"
+            },
+            {
+                "id":"MODEL",
+                "value_name":"Modelo del producto"
+            },
+            {
+                "id":"EAN",
+                "value_name":data.EAN
+            }
+        ]
+    },{
+        headers:{
+            'Authorization': 'Bearer '+ token
+        }
+    })
+    .then(response => { console.log(response)})
+    .catch(error => {
+        console.log(error.response)
+    })
+
+    
+};
 
 module.exports = meliController
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const multer = require("multer")
+const path = require('path');
+//const mimeTypes = require("mime-types")
 const { auth, checkUser, authLogged } = require('../middlewares/auth');
 const userController = require('../controllers/user.js');
 const meliController = require('../controllers/melicontroller.js');
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, path.resolve(__dirname, "../uploads"))
+    }, 
+    filename: function(req,file, callback){
+        callback(null, "tobsfile." + file.fieldname )
+    }
+})
+const upload = multer({ storage: storage })
 
 // Views
 router.get("*", checkUser)
@@ -21,6 +34,7 @@ router.get("/signup", authLogged, (req, res) => {
 
 router.get("/upload", auth, (req, res) => {
     res.cookie('meli', req.query.code, { httpOnly: true });
+    res.sendFile(__dirname + "views/upload.html")
     res.render("upload.html", {token: req.cookies.token})
 })
 
@@ -36,5 +50,7 @@ router.post("/login", userController.login)
 router.get("/logout", auth, userController.logout);
 
 router.post("/upload", meliController.generateTokenAxios)
+
+router.post("/files", upload.single("csv"), meliController.postProducts)
 
 module.exports = router;
