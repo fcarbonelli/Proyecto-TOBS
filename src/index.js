c6ef9267cc109b31651f39cf5d5824c509ad5824
Francisco Carbonelli <carbonelli.francisco@gmail.com>
2021-08-02 23:05:31 -0300

    fixes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express()
 
 dbConnect()
 
-app.set("port",3000)
+app.set("port",process.env.PORT || 3000)
 app.set("views", path.join(__dirname, "views"))
 app.use(express.static(path.join(__dirname, "public")))
 app.use(cookieParser());
